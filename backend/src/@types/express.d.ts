075936183ce5d49ca33682f0a182fa2b37c58981
Abhishek Bishnoi <abhishekbishnoi009988@gmail.com>
2026-02-13 21:04:08 +0530

    Fix express types declaration

--- backend/src/@types/express.d.ts
+++ backend/src/@types/express.d.ts
@@ -1,5 +1,9 @@
-import { Request } from 'express';
+import * as express from 'express';
 
-export interface AuthRequest extends Request {
-  user?: any;
+declare global {
+  namespace Express {
+    interface Request {
+      user?: any;
+    }
+  }
 }
