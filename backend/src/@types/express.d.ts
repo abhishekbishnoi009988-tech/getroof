3c07b7f8b174992e6304cb73ad90f5ca2bf98e30
Abhishek Bishnoi <abhishekbishnoi009988@gmail.com>
2026-02-13 20:43:41 +0530

    Fix custom express types conflict

--- backend/src/@types/express.d.ts
+++ backend/src/@types/express.d.ts
@@ -1,11 +1,5 @@
-import { IUser } from '../models/User';
+import { Request } from 'express';
 
-declare global {
-  namespace Express {
-    interface Request {
-      user?: IUser;
-    }
-  }
+export interface AuthRequest extends Request {
+  user?: any;
 }
-
-export {};
