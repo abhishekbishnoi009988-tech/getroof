875111c12217883cd26f5b4bf8679b30a92fff78
Abhishek Bishnoi <abhishekbishnoi009988@gmail.com>
2026-02-13 21:15:59 +0530

    Fix TypeScript build - remove broken express types

--- backend/src/@types/express.d.ts
+++ backend/src/@types/express.d.ts
@@ -1,9 +1,5 @@
-import * as express from 'express';
-
-declare global {
-  namespace Express {
-    interface Request {
-      user?: any;
-    }
+declare namespace Express {
+  export interface Request {
+    user?: any;
   }
 }
