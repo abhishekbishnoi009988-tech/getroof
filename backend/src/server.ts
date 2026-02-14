c6e35c52bae78bbafa8a0d7a6b09cd6e6c1ce840
Abhishek Bishnoi <abhishekbishnoi009988@gmail.com>
2026-02-14 21:42:32 +0530

    Fix dotenv config in server.ts

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,4 +1,5 @@
-import 'dotenv/config';
+import * as dotenv from 'dotenv';
+dotenv.config();
 import express, { Request, Response, NextFunction } from 'express';
 import cors from 'cors';
 import morgan from 'morgan';
